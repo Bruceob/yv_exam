d6a1f752117450c061ca5a38bb764c76cfe5cdd2
milo <18829297565@163.com>
2018-12-15 10:39:12 +0800

    2

--- conf/settings_testing.py
+++ conf/settings_testing.py
@@ -11,7 +11,7 @@ from settings import APP_ID
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
-        'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
+        'NAME': 'lixiaohua_t',                        # 数据库名 (默认与APP_ID相同)
         'USER': 'root',                            # 你的数据库user
         'PASSWORD': 'Yovole.1',                        # 你的数据库password
         'HOST': '10.3.32.148',                   		   # 数据库HOST
